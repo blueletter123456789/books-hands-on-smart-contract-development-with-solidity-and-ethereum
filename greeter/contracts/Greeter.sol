40f828cc0066bc376c4b1b691eb788910e68f1d9
Shuhei Abe <blueletter123456789@gmail.com>
2022-01-24 22:56:40 +0900

    create hello world function in greeter

--- greeter/contracts/Greeter.sol
+++ greeter/contracts/Greeter.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Greeter{}
+contract Greeter{
+    function greet() external pure returns(string memory){
+        return "Hello, World!";
+    }
+}
