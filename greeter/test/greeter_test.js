fae4968a8372642788965e6b774782100556cc21
Shuhei Abe <blueletter123456789@gmail.com>
2022-01-25 21:29:23 +0900

    modify greeting contract to change dynamically

--- greeter/test/greeter_test.js
+++ greeter/test/greeter_test.js
@@ -9,9 +9,23 @@ contract('Greeter', () => {
   describe('greet()', () => {
     it('returns "Hello, World!"', async () => {
       const greeter = await GreeterContract.deployed()
-      const expocted = 'Hello, World!'
+      const expected = 'Hello, World!'
       const actual = await greeter.greet()
-      assert.equal(actual, expocted, 'greeted with "Hello World!')
+      assert.equal(actual, expected, 'greeted with "Hello World!')
+    })
+  })
+})
+
+contract('Greeter: update greeting', () => {
+  describe('setGreeting(string)', () => {
+    it('sets greeting to passed in string', async () => {
+      const greeter = await GreeterContract.deployed()
+      const expected = 'Hi there'
+
+      await greeter.setGreeting(expected)
+      const actual = await greeter.greet()
+
+      assert.equal(actual, expected, 'greeting was not updated')
     })
   })
 })
